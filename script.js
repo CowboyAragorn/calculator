0c1ce5f7a81e386f8773aed3cdce11cf9767f5cc
CowboyAragorn <Jacobman0313@gmail.com>
2021-12-16 20:36:17 -0500

    clickable display for numbers & clear

---
+++ script.js
@@ -0,0 +1,81 @@
+let screenNumbers = document.querySelector("#screenNumbers");
+let numberZero = document.querySelector("#zero");
+let numberOne = document.querySelector("#one");
+let numberTwo = document.querySelector("#two");
+let numberThree = document.querySelector("#three");
+let numberFour = document.querySelector("#four");
+let numberFive = document.querySelector("#five");
+let numberSix = document.querySelector("#six");
+let numberSeven = document.querySelector("#seven");
+let numberEight = document.querySelector("#eight");
+let numberNine = document.querySelector("#nine");
+numberSeven.value = 7
+
+let numbersDisplayArray = []
+
+//Adds numbers to an array, converts array to a string for display//
+function addToDisplay (x){
+
+    screenNumbers.innerText = " "
+    numbersDisplayArray.push(x);
+    console.log(numbersDisplayArray)
+    screenNumbers.innerText = numbersDisplayArray.join('');
+
+}
+//Event listeners for each button. Probably a more elegant way to do this,
+//but was a simple copy paste//
+
+numberZero.addEventListener("click", () => {
+    addToDisplay(0)
+});
+
+numberOne.addEventListener("click", () => {
+    addToDisplay(1)
+});
+
+numberTwo.addEventListener("click", () => {
+    addToDisplay(2)
+});
+
+numberThree.addEventListener("click", () => {
+    addToDisplay(3)
+});
+
+numberFour.addEventListener("click", () => {
+    addToDisplay(4)
+});
+
+numberFive.addEventListener("click", () => {
+    addToDisplay(5)
+});
+
+numberSix.addEventListener("click", () => {
+    addToDisplay(6)
+});
+
+numberSeven.addEventListener("click", () => {
+    addToDisplay(7)
+});
+
+numberEight.addEventListener("click", () => {
+    addToDisplay(8)
+});
+
+
+numberNine.addEventListener("click", () => {
+    addToDisplay(9)
+});
+
+//Sets up clear button to erase the visible screen. Will need to come back
+//to erase hidden operation arrays//
+let clearButton = document.querySelector('#clear')
+
+function clearAll(){
+    numbersDisplayArray = []
+    screenNumbers.innerText = numbersDisplayArray
+}
+
+clearButton.addEventListener('click', clearAll)
+
+
+
